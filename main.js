0625755287ee46a8dc1a8c603bb91c96f50ee806
Geoffrey <gboss4@gmail.com>
2013-11-20 17:53:20 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function displayTodo(){
+	var formInput = document.getElementById('theInput').value;
+	document.getElementById('newText').innerHTML = formInput ;
+
+function insert() {
+	formInput.push( newText.value );
+
+	clearAndShow();
+	}}
